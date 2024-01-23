db91c10bd7d8dbe0902df2362525fde0743dded2
Iani <i_jirahmae@me.com>
2024-01-23 08:21:00 -0200

    Added MR and US separate dataloaders

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -6,6 +6,7 @@ from torch.utils.data import DataLoader, Dataset
 import torch 
 
 class MR_US_dataset(Dataset):
+    
     """
     Dataset that acquires the following: 
     MR
@@ -13,6 +14,7 @@ class MR_US_dataset(Dataset):
     US
     US_label 
     """
+    
     def __init__(self, dir_name, mode = 'train', downsample = False, alligned = False):
         self.dir_name = dir_name 
         self.mode = mode 
@@ -117,11 +119,243 @@ class MR_US_dataset(Dataset):
         
         return norm_img 
     
+
+class US_dataset(Dataset):
+    
+    def __init__(self, dir_name, mode = 'train', downsample = False, alligned = False):
+        self.dir_name = dir_name 
+        self.mode = mode 
+        
+        # obtain list of names for us and mri labels 
+        self.us_names = os.listdir(os.path.join(dir_name, mode, 'us_images'))
+        self.us_label_names = os.listdir(os.path.join(dir_name, mode, 'us_labels'))
+        
+        # self.mri_names = os.listdir(os.path.join(dir_name, mode, 'mr_images'))
+        # self.mri_label_names = os.listdir(os.path.join(dir_name, mode, 'mr_labels'))
+        self.num_data = len(self.us_names)
+        self.downsample = downsample
+        self.alligned = alligned
+        
+        # Load items 
+        self.us_data = [torch.tensor(nib.load(os.path.join(dir_name, mode, 'us_images', self.us_names [i])).get_fdata().squeeze()) for i in range(self.num_data)]
+        self.us_labels = [torch.tensor(nib.load(os.path.join(dir_name, mode,'us_labels', self.us_label_names[i])).get_fdata()) for i in range(self.num_data)]
+        #self.mri_data = [torch.tensor(nib.load(os.path.join(dir_name, mode, 'mr_images', self.mri_names [i])).get_fdata().squeeze()) for i in range(self.num_data)]
+        #self.mri_labels = [torch.tensor(nib.load(os.path.join(dir_name, mode, 'mr_labels', self.mri_label_names [i])).get_fdata().squeeze()) for i in range(self.num_data)]
+        
+    def __len__(self):
+        return self.num_data
+        
+    def __getitem__(self, idx):
+        
+        #upsample_us = self.resample(self.us_data[idx])
+        #upsample_us_labels = self.resample(self.us_labels[idx], label = True)
+        
+        if self.alligned:
+            # no need to transpose if alligned alerady
+            t_us = self.us_data[idx]
+            t_us_labels = self.us_labels[idx]
+        else: # Change view of us image to match mr first 
+            t_us = torch.transpose(self.us_data[idx], 2,0)
+            t_us_labels = torch.transpose(self.us_labels[idx], 2,0)
+            
+        # Upsample to MR images 
+        upsample_us = self.resample(t_us)
+        upsample_us_labels = self.resample(t_us_labels, label = True)
+        
+        # Squeeze dimensions 
+        us = upsample_us.squeeze().unsqueeze(0)
+        us_label = upsample_us_labels.squeeze().unsqueeze(0)
+        
+        # normalise data 
+        #mr = self.normalise_data(mr_data)
+        #mr_label = self.normalise_data(mr_label)
+        us = self.normalise_data(us)
+        us_label = self.normalise_data(us_label)
+        
+        # if resample is true 
+        if self.downsample: 
+            upsample_method = torch.nn.Upsample(size = (64,64,64))
+            #mr = upsample_method(mr.unsqueeze(0)).squeeze(0)
+            #mr_label = upsample_method(mr_label.unsqueeze(0)).squeeze(0)
+            us = upsample_method(us.unsqueeze(0)).squeeze(0)
+            us_label = upsample_method(us_label.unsqueeze(0)).squeeze(0)
+        
+        
+        # Sample a slice and corresponding label 
+        us_shape = us.size()
+        
+        # randomly sample US slice 
+        slice_idx = np.random.choice(np.arange(0,us_shape[-1]))
+        #print(f"Slice idx {slice_idx}")
+        us_slice = us[:,:,slice_idx]
+        us_label_slice = us_label[:,:,slice_idx] # segmented slice 
+        us_label_classify = torch.tensor(1.0*(len(torch.unique(us_label_slice)) > 1)) # 1 if presence of prostate 
+        
+        return us_slice, us_label_slice, us_label_classify
+    
+    def resample(self, img, dims = (120,128,128), label = False):
+        upsample_method = torch.nn.Upsample(size = dims)
+        if label: 
+            
+            if len(img.size()) > 3:
+                img_label = img[:,:,:,0]
+            else:
+                img_label = img 
+                
+            # Choose only prostate gland label 
+            if len(img.size()) > 3:
+                img_label = img[:,:,:,0]
+            else:
+                img_label = img 
+                
+            upsampled_img = upsample_method(img_label.unsqueeze(0).unsqueeze(0))
+        else:
+            upsampled_img = upsample_method(img.unsqueeze(0).unsqueeze(0))
+        
+        return upsampled_img
+    
+    def normalise_data(self, img):
+        """
+        Normalises labels and images 
+        """
+        
+        min_val = torch.min(img)
+        max_val = torch.max(img)
+        
+        if max_val == 0: 
+            #print(f"Empty mask, not normalised img")
+            norm_img = img # return as 0s only if blank image or volume 
+        else: 
+            norm_img = (img - min_val) / (max_val - min_val)
+        
+        return norm_img       
+
+
+class MR_dataset(Dataset):
+    
+    def __init__(self, dir_name, mode = 'train', downsample = False, alligned = False):
+        self.dir_name = dir_name 
+        self.mode = mode 
+        
+        # obtain list of names for us and mri labels 
+        #self.us_names = os.listdir(os.path.join(dir_name, mode, 'us_images'))
+        #self.us_label_names = os.listdir(os.path.join(dir_name, mode, 'us_labels'))
+        
+        self.mri_names = os.listdir(os.path.join(dir_name, mode, 'mr_images'))
+        self.mri_label_names = os.listdir(os.path.join(dir_name, mode, 'mr_labels'))
+        self.num_data = len(self.mri_names)
+        self.downsample = downsample
+        self.alligned = alligned
+        
+        # Load items 
+        #self.us_data = [torch.tensor(nib.load(os.path.join(dir_name, mode, 'us_images', self.us_names [i])).get_fdata().squeeze()) for i in range(self.num_data)]
+        #self.us_labels = [torch.tensor(nib.load(os.path.join(dir_name, mode,'us_labels', self.us_label_names[i])).get_fdata()) for i in range(self.num_data)]
+        self.mri_data = [torch.tensor(nib.load(os.path.join(dir_name, mode, 'mr_images', self.mri_names [i])).get_fdata().squeeze()) for i in range(self.num_data)]
+        self.mri_labels = [torch.tensor(nib.load(os.path.join(dir_name, mode, 'mr_labels', self.mri_label_names [i])).get_fdata().squeeze()) for i in range(self.num_data)]
+        
+    def __len__(self):
+        return self.num_data
+        
+    def __getitem__(self, idx):
+        
+        #upsample_us = self.resample(self.us_data[idx])
+        #upsample_us_labels = self.resample(self.us_labels[idx], label = True)
+        
+        # if self.alligned:
+        #     # no need to transpose if alligned alerady
+        #     t_us = self.us_data[idx]
+        #     t_us_labels = self.us_labels[idx]
+        # else: # Change view of us image to match mr first 
+        #     t_us = torch.transpose(self.us_data[idx], 2,0)
+        #     t_us_labels = torch.transpose(self.us_labels[idx], 2,0)
+            
+        # Upsample to MR images 
+        #upsample_us = self.resample(t_us)
+        #upsample_us_labels = self.resample(t_us_labels, label = True)
+        
+        # Squeeze dimensions 
+        #us = upsample_us.squeeze().unsqueeze(0)
+        #us_label = upsample_us_labels.squeeze().unsqueeze(0)
+        
+        # Add dimesion for "channel"
+        mr_data = self.mri_data[idx].unsqueeze(0)
+        mr_label = self.mri_labels[idx].unsqueeze(0)
+        
+        # normalise data 
+        mr = self.normalise_data(mr_data)
+        mr_label = self.normalise_data(mr_label)
+        #us = self.normalise_data(us)
+        #us_label = self.normalise_data(us_label)
+        
+        # if resample is true 
+        if self.downsample: 
+            upsample_method = torch.nn.Upsample(size = (64,64,64))
+            mr = upsample_method(mr.unsqueeze(0)).squeeze(0)
+            mr_label = upsample_method(mr_label.unsqueeze(0)).squeeze(0)
+            #us = upsample_method(us.unsqueeze(0)).squeeze(0)
+            #us_label = upsample_method(us_label.unsqueeze(0)).squeeze(0)
+        
+        # Sample a slice and corresponding label 
+        #us_shape = us.size()
+        mr_shape = mr.size()
+        
+        # randomly sample US slice 
+        slice_idx = np.random.choice(np.arange(0,mr_shape[-1]))
+        
+        #print(f"Slice idx {slice_idx}")
+        mr_slice = mr[:,:,slice_idx]
+        mr_label_slice = mr_label[:,:,slice_idx] # segmented slice 
+        mr_label_classify = torch.tensor(1.0*(len(torch.unique(mr_label_slice)) > 1)) # 1 if presence of prostate 
+        
+        return mr_slice, mr_label_slice, mr_label_classify
+    
+    def resample(self, img, dims = (120,128,128), label = False):
+        upsample_method = torch.nn.Upsample(size = dims)
+        if label: 
+            
+            if len(img.size()) > 3:
+                img_label = img[:,:,:,0]
+            else:
+                img_label = img 
+                
+            # Choose only prostate gland label 
+            if len(img.size()) > 3:
+                img_label = img[:,:,:,0]
+            else:
+                img_label = img 
+                
+            upsampled_img = upsample_method(img_label.unsqueeze(0).unsqueeze(0))
+        else:
+            upsampled_img = upsample_method(img.unsqueeze(0).unsqueeze(0))
+        
+        return upsampled_img
+    
+    def normalise_data(self, img):
+        """
+        Normalises labels and images 
+        """
+        
+        min_val = torch.min(img)
+        max_val = torch.max(img)
+        
+        if max_val == 0: 
+            #print(f"Empty mask, not normalised img")
+            norm_img = img # return as 0s only if blank image or volume 
+        else: 
+            norm_img = (img - min_val) / (max_val - min_val)
+        
+        return norm_img       
+
+
+
 if __name__ == '__main__':
     
-    data_folder = '/Users/ianijirahmae/Documents/DATASETS/mri_us_paired'
-    train_dataset = MR_US_dataset(data_folder, mode = 'train')
+    #data_folder = '/Users/ianijirahmae/Documents/DATASETS/mri_us_paired'
+    data_folder = './Data'
+    train_dataset = US_dataset(data_folder, mode = 'train')
     train_dataloader = DataLoader(train_dataset)
+    
+    us, us_label = train_dataset[0]
     
     for idx, (mr, us, mr_label, us_label) in enumerate(train_dataset):
         print(idx)
